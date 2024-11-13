b037fa463528baf311c69d2b4ff35a16a8629ad4
baaysen <baaysen@gmail.com>
2024-11-13 02:17:57 +0100

    Update streamlit_projet.py

--- streamlit_projet.py
+++ streamlit_projet.py
@@ -78,7 +78,7 @@ if page == pages[0] :
   st.write("- Augementation des températures moyennes depuis plus d'un siècle")
   st.write("- Causé en grande partie par les activités humaine")
   st.write("- Modification des écosystèmes et des équilibres météorologie")
-  st.image(r"https://github.com/baaysen/Global-Temperature-Analysis/intro.gif", caption="Illustration de l'introduction")
+  st.image(r"https://giphy.com/gifs/nasa-nasagif-polar-vortex-polarvortex2019-1NZVjc68MgkT4X8BQi", caption="Illustration de l'introduction")
   #st.image(r"C:/Users/diakh/Bureau/DA_course/Projet_DA/datas/intro.gif", caption="Illustration de l'introduction")
     
 
